a1386ee6bb13233cf315febe5041e8a3ef1b98de
mehhtadeepen <mehta.de@husky.neu.edu>
2016-01-20 13:35:18 -0500

    Changed Server using static

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ var app = express();
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
-//app.get('/', index.html);
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+    res.render('index.html');
+});
+
 
 app.get('/hello', function(req, res){
   res.send('hello world');
