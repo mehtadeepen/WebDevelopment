c98b363030627c1416d64a18db3d799e529d866f
mehhtadeepen <mehta.de@husky.neu.edu>
2016-01-22 00:17:03 -0500

    Configured Auth with mailgun

--- server.js
+++ server.js
@@ -18,44 +18,76 @@ app.get('/hello', function(req, res){
 });
 
 var nodemailer = require('nodemailer');
+var mg = require('nodemailer-mailgun-transport');
 
-function handleSayHello(req, res) {
-    // Not the movie transporter!
-    var transporter = nodemailer.createTransport({
-        service: 'Gmail',
-        auth: {
-            user: 'webdevelopment.mehtadeepen@gmail.com', // Your email id
-            pass: 'webdevelopment' // Your password
-        }
-    });
-     var textbody = req.body;
+function mailMe(req, res) {
+    var auth = {
+      auth: {
+          api_key: 'key-c677dcdd00575919169154f293704b47',
+          domain: 'sandbox9efde8dec1264a6c993d2fa969836644.mailgun.org'
+      }
+    };
+
+    var transporter = nodemailer.createTransport(mg(auth));
 
     var mailOptions = {
-    from: 'webdevelopment.mehtadeepen@gmail.com', // sender address
-    to: 'mr.san.kumar@gmail.com', // list of receivers
-    subject: 'Email Example', // Subject line
-    //text: textbody
-    html: '<b>Hello world 🐴</b>' //, // plaintext body
-
-   
-    // html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
-};
-
-
-transporter.sendMail(mailOptions, function(error, info){
-    if(error){
-        console.log(error);
-        res.json({yo: 'error'});
-    }else{
-        console.log('Message sent: ' + info.response);
-        res.json({yo: info.response});
+        from: 'Support <webdevelopment.mehtadeepen@gmail.com>',
+        to: 'Support <deepenmehta27@gmail.com>',
+        subject: 'The subject',
+        text: 'Message in plain text', // plaintext body
+        html: '<b>Hello</b>' // html body
     };
-});
 
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+            console.log(error);
+        }else{
+            console.log('Message sent: ' + info.response);
+            res.json({yo: info.response});
+        }
+    });
 }
 
+// var nodemailer = require('nodemailer');
 
-app.get('/sayHello', handleSayHello);
+// function handleSayHello(req, res) {
+//     // Not the movie transporter!
+//     var transporter = nodemailer.createTransport({
+//         service: 'Gmail',
+//         auth: {
+//             user: 'webdevelopment.mehtadeepen@gmail.com', // Your email id
+//             pass: 'webdevelopment' // Your password
+//         }
+//     });
+//      var textbody = req.body;
+
+//     var mailOptions = {
+//     from: 'webdevelopment.mehtadeepen@gmail.com', // sender address
+//     to: 'mr.san.kumar@gmail.com', // list of receivers
+//     subject: 'Email Example', // Subject line
+//     //text: textbody
+//     html: '<b>Hello world 🐴</b>' //, // plaintext body
+
+   
+//     // html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
+// };
+
+
+// transporter.sendMail(mailOptions, function(error, info){
+//     if(error){
+//         console.log(error);
+//         res.json({yo: 'error'});
+//     }else{
+//         console.log('Message sent: ' + info.response);
+//         res.json({yo: info.response});
+//     };
+// });
+
+// }
+
+
+ app.get('/sayHello', mailMe);
 //router.post('/', handleSayHello); // handle the route at yourdomain.com/sayHello
 
 //var text = 'Hello world from \n\n' + req.body.name;
