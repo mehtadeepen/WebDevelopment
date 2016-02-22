31dad48c92f9d5b9f702a63cfb8e1d4908995e95
mehhtadeepen <mehta.de@husky.neu.edu>
2016-02-22 08:53:51 -0500

    Added Angular components

---
+++ public/assignment/services/user.service.client.js
@@ -0,0 +1,96 @@
+(function(){
+
+    angular
+        .module("FormBuilderApp")
+        .factory("UserService", UserService);
+
+    function UserService () {
+
+        var users = [
+            {        "_id":123, "firstName":"Alice",            "lastName":"Wonderland",
+                "username":"alice",  "password":"alice",   "roles": ["student"]                },
+            {        "_id":234, "firstName":"Bob",              "lastName":"Hope",
+                "username":"bob",    "password":"bob",     "roles": ["admin"]                },
+            {        "_id":345, "firstName":"Charlie",          "lastName":"Brown",
+                "username":"charlie","password":"charlie", "roles": ["faculty"]                },
+            {        "_id":456, "firstName":"Dan",              "lastName":"Craig",
+                "username":"dan",    "password":"dan",     "roles": ["faculty", "admin"]},
+            {        "_id":567, "firstName":"Edward",           "lastName":"Norton",
+                "username":"ed",     "password":"ed",      "roles": ["student"]                }
+        ];
+
+        var api = {
+            findUserByCredentials : findUserByCredentials,
+            findAllUsers : findAllUsers,
+            createUser : createUser,
+            deleteUserById : deleteUserById,
+            updateUser : updateUser
+        };
+
+        return api;
+
+        function findUserByCredentials(username, password, callback) {
+             var user = null;
+            for (var i = 0; i < users.length; i++) {
+                if(users[i].username === username && users[i].password === password){
+                    user =  users[i]
+                }
+            }
+            return user;
+        }
+
+        function findAllUsers (callback) {
+            callback(users);
+        }
+
+        function createUser (user, callback) {
+            var id = (new Date).getTime();
+            var newUser = {
+                "_id" : id,
+                "firstName" : "",
+                "lastName" : "",
+                "username" : user.username,
+                "password" : user.password,
+                "email" : user.email,
+                "roles" : []
+            }
+            users.push(newUser);
+            callback(newUser);
+        }
+
+        function deleteUserById (userId, callback) {
+            var userIndex = getUserIndexById(userId);
+            users.splice(userIndex, 1);
+            callback(users);
+        }
+
+        function updateUser (userId, user, callback) {
+            console.log(user);
+            var userIndex = getUserIndexById(userId);
+            users[userIndex] = {
+                "_id" : user._id,
+                "firstName" : user.firstName,
+                "lastName" : user.lastName,
+                "username" : user.username,
+                "password" : user.password,
+                "roles" : user.roles,
+                "email" : user.email
+            }
+            callback(users[userIndex]);
+        }
+
+        function getUserIndexById (userId) {
+            var index = 0;
+            for (var i = 0; i < users.length; i++) {
+                if(users[i]._id === userId){
+                    return index;
+                }
+                index++;
+            }
+        }
+
+        f
+
+    }
+
+})();
---
+++ public/assignment/views/users/login.controller.js
@@ -0,0 +1,11 @@
+(function() {
+
+    angular
+        .module("FormBuilderApp")
+        .controller("LoginController", LoginController);
+
+    function LoginController($scope, $location){
+        $scope.$location = $location;
+    }
+
+})();
